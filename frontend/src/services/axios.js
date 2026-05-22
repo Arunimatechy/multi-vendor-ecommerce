6707e4dde11da145e73603e1f19f656ee3ff7b3b
Arunima <arunimanairs91@gmail.com>
2026-05-22 20:41:35 +0530

    Fixed axios production setup

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,59 +1,90 @@
-
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+
 });
 
 // ================= REQUEST INTERCEPTOR =================
+
 API.interceptors.request.use(
+
   (config) => {
+
     const token = localStorage.getItem("access");
 
     if (token) {
+
       config.headers.Authorization = `Bearer ${token}`;
+
     }
 
     return config;
+
   },
+
   (error) => Promise.reject(error)
+
 );
 
 // ================= RESPONSE INTERCEPTOR =================
+
 API.interceptors.response.use(
+
   (response) => response,
 
   async (error) => {
+
     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if (
+
+      error.response?.status === 401 &&
+      !originalRequest._retry
+
+    ) {
+
       originalRequest._retry = true;
 
       try {
+
         const refresh = localStorage.getItem("refresh");
 
         const res = await axios.post(
-          "http://127.0.0.1:8000/api/token/refresh/",
+
+          `${import.meta.env.VITE_API_URL}/api/token/refresh/`,
+
           { refresh }
+
         );
 
         const newAccess = res.data.access;
 
-        localStorage.setItem("access", newAccess);
+        localStorage.setItem(
+          "access",
+          newAccess
+        );
 
-        originalRequest.headers.Authorization = `Bearer ${newAccess}`;
+        originalRequest.headers.Authorization =
+          `Bearer ${newAccess}`;
 
         return API(originalRequest);
+
       } catch (err) {
-        // hard logout
+
         localStorage.clear();
+
         window.location.href = "/login";
+
       }
+
     }
 
     return Promise.reject(error);
+
   }
+
 );
 
 export default API;
