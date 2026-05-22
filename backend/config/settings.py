aa982ca0753c76fb3fc2fe56531fec527dd21148
Arunima <arunimanairs91@gmail.com>
2026-05-22 20:52:23 +0530

    Fixed production database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -189,13 +189,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 # DATABASE
 # ======================================================
 
+# ======================================================
+# DATABASE
+# ======================================================
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL")
     )
 }
-
 
 # ======================================================
 # PASSWORD VALIDATION
